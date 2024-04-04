9b5690fe319fe297f89ca19e2b982003c9896b6f
abhinavg760@gmail.com <abhinavg760@gmail.com>
2024-04-04 23:09:57 +0530

    new

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -xe
+
+clang -Wall -Wextra -ggdb -o main main.c
+
---
+++ main.c
@@ -0,0 +1,53 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+float dataset[][2]=
+{   {0,0},
+    {1,2},
+    {2,4},
+    {3,6},
+    {4,8},
+};
+#define train_count (sizeof(dataset)/sizeof(dataset[0]))
+float rand_float(void)
+{
+    return (float) rand()/(float) RAND_MAX;
+}
+
+float cost(float w,float b)
+{
+  float result=0.0;
+  for(size_t i=0 ; i<train_count ; i++)
+    {
+        float x=dataset[i][0];
+        float y=x*w+b;
+        float error = y-dataset[i][1];
+        result+=error*error;
+    }
+  result/=train_count;
+  return result;
+}
+
+int main()
+{
+    srand(time(0));
+    float eps=1e-3;
+    float rate=1e-3;
+    
+    //y=w*x;
+    float w=rand_float()*10.0f;
+    float b=rand_float()*5.0f;
+    for(size_t i=0;i<500;++i)
+      {
+	float c = cost(w,b);
+	float dw=(cost(w+eps,b)-c)/eps;
+	float db=(cost(w,eps+b)-c)/eps;
+	w-=rate*dw;
+	b-=rate*db;
+	printf("cost= %f , w=%f ,b=%f \n",cost(w,b),w,b);
+      }
+    
+    printf("-----------------------------------------\n");
+    printf("w=%f , b=%f\n",w,b);
+    return 0;
+}
